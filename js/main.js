895c71f2cbba3cf9a7fcfd9d5515be2695a77aac
Filippo Pittau <filippo.pittau@gmail.com>
2020-03-01 18:28:47 +0100

    Commit 3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
 
-    $('.menu-item').click(function() {
-        $(".dropdown-menu").slideToggle();
-
+    $('.menu-item.course').click(function() {
+        //$(".dropdown-menu.course").slideDown();//cosi apre e chiude contemporaneamente( abassa)
+        //$('.dropdown-menu.course').slideUp();//cosi apre e chiude contemporaneamente (chiude)
+        $(".dropdown-menu.course").slideToggle();//questo fa si che il menu 'dropdown-menu.course
+                                          // si apra con un clicke si chiuda con un 'altro click'
      });
+     $('.menu-item.after-course').click(function(){//idem come sopra su classi diverse per diversi menu
+         $('.dropdown-menu.after-course').slideToggle();
+     });
+
 });
