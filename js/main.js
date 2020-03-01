83cf505a0364c0f44caec72a4852143eaf74d52f
Filippo Pittau <filippo.pittau@gmail.com>
2020-03-01 19:05:42 +0100

    Commit 4

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         //$('.dropdown-menu.course').slideUp();//cosi apre e chiude contemporaneamente (chiude)
         $(".dropdown-menu.course").slideToggle();//questo fa si che il menu 'dropdown-menu.course
                                           // si apra con un clicke si chiuda con un 'altro click'
+        $('#f107').addClass('ruota');
      });
      $('.menu-item.after-course').click(function(){//idem come sopra su classi diverse per diversi menu
          $('.dropdown-menu.after-course').slideToggle();
